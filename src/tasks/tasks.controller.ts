c78fd11846edcf72973a2b69d60fbe60031b69c9
mario-galindo <mgalindo9410@gmail.com>
2021-11-25 14:51:17 -0600

    playing around with new controller and routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get()
+  getTasks(): string {
+    return 'Retrieven tasks...';
+  }
+
+  @Post()
+  createTask(): string {
+    return 'Creating task...';
+  }
+}
