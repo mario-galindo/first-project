6b839419762a362e1f667cac8e1047e167f65fbd
mario-galindo <mgalindo9410@gmail.com>
2021-11-26 14:50:00 -0600

    service added to return a list of tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './interfaces/Task';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Task Sample 1',
+      description: 'only for sample purpose',
+      done: true,
+    },
+    {
+      id: 2,
+      title: 'Task Sample 2',
+      description: 'only for sample purpose',
+      done: true,
+    },
+    {
+      id: 3,
+      title: 'Task Sample 3',
+      description: 'only for sample purpose',
+      done: true,
+    },
+  ];
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTask(id: number): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+}
