b66e5717f28e42452cf45886a0479e90e458d834
mario-galindo <mgalindo9410@gmail.com>
2021-11-26 18:59:33 -0600

    returning to dummy data

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,5 +3,32 @@ import { Task } from './interfaces/Task';
 
 @Injectable()
 export class TasksService {
-  
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Task Sample 1',
+      description: 'only for sample purpose',
+      done: true,
+    },
+    {
+      id: 2,
+      title: 'Task Sample 2',
+      description: 'only for sample purpose',
+      done: true,
+    },
+    {
+      id: 3,
+      title: 'Task Sample 3',
+      description: 'only for sample purpose',
+      done: true,
+    },
+  ];
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTask(id: number): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
 }
