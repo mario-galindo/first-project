b255be1b46ba93243d74e33d4302485047b0d306
mario-galindo <mgalindo9410@gmail.com>
2021-11-26 18:12:01 -0600

    task schema added

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { taskSchema } from './schemas/task.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'Task', schema: taskSchema }])],
   controllers: [TasksController],
   providers: [TasksService],
 })
